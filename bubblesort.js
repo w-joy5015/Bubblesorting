47e7a42eade567e79ffd711e67fd2ea9feab7962
Olesia Guidi <olgarbuzova@gmail.com>
2019-10-22 12:40:20 -0400

    sorted func

--- bubblesort.js
+++ bubblesort.js
@@ -4,6 +4,15 @@ function swap(arr, a,b){
   arr[b]=temp
 }
 
+function sorted(arr,a,b){
+  if(arr[a]>arr[b]) {
+    return -1
+  }
+  else{
+    return 1
+}
+}
+
 function bubbleSort(array) {
   if(array.length===0|| array.length===1){
     return array
@@ -12,7 +21,7 @@ function bubbleSort(array) {
   else{
     for(let i=0; i<array.length-1; i++){
       for (let j=0;j<array.length-i-1;j++){
-        if(array[j+1]<array[j]){
+        if(sorted(array,j,j+1)===-1){
           swap(array,j, j+1);
       }
  }
